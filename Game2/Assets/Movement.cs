1bda7d5099f1824b466910ba86058cc23c9fb09a
alieftwu <alieftwu@gmail.com>
2024-02-26 18:09:03 -0600

    Player Animation & Scripts

--- Game2/Assets/Movement.cs
+++ Game2/Assets/Movement.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float movSpeed;
-    float speedX, speedY;
-    Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float speed; 
+    private Rigidbody2D rb;
+    private Vector2 movInput;
+    public Animator animator;
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Horizontal", movInput.x);
+        animator.SetFloat("Vertical", movInput.y);
+        animator.SetFloat("Speed", movInput.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = movInput * speed;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputValue inputValue)
     {
-        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
-        rb.velocity = new Vector2(speedX, speedY);
-        
+        movInput = inputValue.Get<Vector2>();
     }
 }
